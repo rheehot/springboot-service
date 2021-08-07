f2f093521777eec3fdc821bb83b476ce568866de
HYEBIN,PARK <hyeqls3742@gmail.com>
2021-08-07 18:22:39 +0900

    등록 구현, 그래들 버전 업그레이드

--- src/main/java/com/hyeb/book/springboot/Application.java
+++ src/main/java/com/hyeb/book/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book.springboot;
+package com.hyeb.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/hyeb/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/hyeb/book/springboot/web/dto/HelloResponseDto.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book.springboot.web.dto;
+package com.hyeb.book.springboot.web.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
