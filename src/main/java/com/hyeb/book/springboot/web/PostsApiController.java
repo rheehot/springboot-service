139c07d59312a5fb42d7cec6afd6fba12929fed5
HYEBIN,PARK <hyeqls3742@gmail.com>
2021-08-08 17:49:46 +0900

    error 해결

--- src/main/java/com/hyeb/book/springboot/web/PostsApiController.java
+++ src/main/java/com/hyeb/book/springboot/web/PostsApiController.java
@@ -2,11 +2,11 @@ package com.hyeb.book.springboot.web;
 
 
 import com.hyeb.book.springboot.service.posts.PostsService;
+import com.hyeb.book.springboot.web.dto.PostsResponseDto;
 import com.hyeb.book.springboot.web.dto.PostsSaveRequestDto;
+import com.hyeb.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,5 +17,16 @@ public class PostsApiController {
     public Long save(
             @RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody
+            PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
