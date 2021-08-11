850fde685bc4da1f6f744cc5975ddecba4a7e95a
HYEBPARK <hyeqls3742@gmail.com>
2021-08-11 19:15:13 +0900

    구글 로그인, 로그아웃, 회원가입 ,권한관리 기능 구현

--- src/main/java/com/hyeb/book/springboot/web/IndexController.java
+++ src/main/java/com/hyeb/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hyeb.book.springboot.web;
 
+import com.hyeb.book.springboot.config.auth.dto.SessionUser;
 import com.hyeb.book.springboot.service.posts.PostsService;
 import com.hyeb.book.springboot.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,16 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -26,6 +35,7 @@ public class IndexController {
     public String postsSave(){
         return "postsSave";
     }
+
 
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model){
